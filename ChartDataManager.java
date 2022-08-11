6842ecbedac9d4ed2938b6534f516ed8da42e7a3
alosadasei <110858361+alosadasei@users.noreply.github.com>
2022-08-11 19:24:42 +0200

    ChartDataManager
    Here you have an example of how create a Json to draw your chart.

---
+++ ChartDataManager.java
@@ -0,0 +1,47 @@
+import java.io.Writer;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+
+public class ChartDataManager extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException{
+		try {
+			response.setCharacterEncoding("utf-8");
+      ArrayList<Object> list = new ArrayList<Object>(); //Load this ArrayList with the data you need
+			Writer writer = response.getWriter();
+			JsonObject chartData = new JsonObject();
+      JsonArray labels = new JsonArray();
+      JsonArray datasets = new JsonArray();
+      JsonObject dataset = new JsonObject();
+      JsonArray data = new JsonArray();
+      for(Object object : list){  //Use this for to push the values
+        labels.add(object.getLabel()); //Insert columns into labels
+        data.add(object.getData());    //Inser rows into data
+      }
+      String nameOfData = "", lineColor = "";
+      dataset.add("data", data);
+		  dataset.addProperty("label", nameOfData);
+		  dataset.addProperty("borderColor", lineColor);
+		  dataset.addProperty("fill", false);
+      //If you want to make a comparative chart, you'll need more than one dataset
+		  datasets.add(dataset);
+      chartData.add("labels", labels);
+		  chartData.add("datasets", datasets);
+      writer.write(chartData.toString();
+			writer.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
